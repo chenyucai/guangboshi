d1921fb2b96f3cd8008e8cc9b579b09075d21389
chenyucai <e_ycchen@126.com>
2017-02-11 15:45:59 +0800

    init

---
+++ home/js/signin.js
@@ -0,0 +1,13 @@
+//$.ajax({
+//	cache: true,
+//	type: "POST",
+//	url: ajaxCallUrl,
+//	data: $('#yourformid').serialize(), // 你的formid
+//	async: false,
+//	error: function(request) {
+//		alert("Connection error");
+//	},
+//	success: function(data) {
+//		$("#commonLayout_appcreshi").parent().html(data);
+//	}
+//});
