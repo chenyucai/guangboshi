8c39c7871d3eac38a86c88bc14f02c44e0e54094
chenyucai <e_ycchen@126.com>
2017-03-20 15:41:21 +0800

    up

--- beautifulLibrary/js/comment.js
+++ beautifulLibrary/js/comment.js
@@ -5,9 +5,12 @@ var recordId = localStorage.getItem("commentGoodId");
 var title = localStorage.getItem("commentGoodName");
 var ctgName = localStorage.getItem("commentCategory");
 
+var cType = localStorage.getItem('cType');
+
 var vm = new Vue({
   el: '#app',
   data: {
+    cType: cType,
     category: category,
     recordId: recordId,
     ctgName:ctgName,
@@ -51,35 +54,38 @@ var vm = new Vue({
         return false;
       }
       // this.images.push('img/star-l@2x.png');
-      var file = e.target.files[0]; //获取上传文件列表中第一个文件
-  		if (!/image\/\w+/.test(file.type)) {
-  			//图片文件的type值为image/png或image/jpg
-  			alert("文件必须为图片！");
-  			return false;
-  		}
-  		var reader = new FileReader(); //实例一个文件对象
-  		reader.readAsDataURL(file); //把上传的文件转换成url
-  		//当文件读取成功便可以调取上传的接口
-  		reader.onload = function(e) {
-  			console.log(e.target.result); //文件路径
-  			// _this.images.push(e.target.result);
-        var str = e.target.result.split(',')[1];
-        $.ajax({
-          url:'http://www.missnefer.com/AppAssess/UploadPicture',
-          type:'POST',
-          // contentType:'application/json;charset=utf-8',
-          // data: JSON.stringify(data),
-          // traditional:true,
-          data:{
-            userId: userId,
-            recordId: recordId,
-            ImageStr:str
-          },
-          success: function(res){
-            _this.images.push(res);
-          }
-        });
-  		};
+      console.log(e.target.files.length);
+      for (var i = 0; i < e.target.files.length; i++) {
+        var file = e.target.files[i]; //获取上传文件列表中第i个文件
+    		if (!/image\/\w+/.test(file.type)) {
+    			//图片文件的type值为image/png或image/jpg
+    			alert("文件必须为图片！");
+    			return false;
+    		}
+    		var reader = new FileReader(); //实例一个文件对象
+    		reader.readAsDataURL(file); //把上传的文件转换成url
+    		//当文件读取成功便可以调取上传的接口
+    		reader.onload = function(e) {
+    			// console.log(e.target.result); //文件路径
+    			// _this.images.push(e.target.result);
+          var str = e.target.result.split(',')[1];
+          $.ajax({
+            url:'http://www.missnefer.com/AppAssess/UploadPicture',
+            type:'POST',
+            // contentType:'application/json;charset=utf-8',
+            // data: JSON.stringify(data),
+            // traditional:true,
+            data:{
+              userId: userId,
+              recordId: recordId,
+              ImageStr:str
+            },
+            success: function(res){
+              _this.images.push(res);
+            }
+          });
+    		};
+      }
     },
     submit: function(){
       if (this.content == '') {
@@ -104,7 +110,10 @@ var vm = new Vue({
           list.push(obj);
         });
       });
-      data.marks = list;
+      if (this.cType == '1') {
+        data.marks = list;
+      }
+
       $.ajax({
         url:'http://www.missnefer.com/AppAssess/SaveAssessItemList',
         type:'POST',
@@ -114,7 +123,8 @@ var vm = new Vue({
         // data:data,
         success: function(res){
           if (res == "") {
-            alert('发表成功！')
+            alert('发表成功！');
+            window.history.go(-1);
           } else {
             alert(res);
           }
